7050eaed1485c0ac4b742acbdf9a357341aa3b9b
ZV-IHA <vad.zaharkin@yandex.ru>
2021-05-16 22:53:55 +0300

    121221

--- app/src/main/java/com/example/autoavto/ui/activities/CreateNotePadActivity.java
+++ app/src/main/java/com/example/autoavto/ui/activities/CreateNotePadActivity.java
@@ -2,7 +2,9 @@ package com.example.autoavto.ui.activities;
 
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.FileUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,15 +12,22 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.autoavto.R;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-
+import java.util.Scanner;
 
 
 public class CreateNotePadActivity extends AppCompatActivity {
@@ -28,6 +37,7 @@ public class CreateNotePadActivity extends AppCompatActivity {
     EditText noteText;
     ImageButton buttonBack;
     int b = 1;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,9 +53,17 @@ public class CreateNotePadActivity extends AppCompatActivity {
         if (getIntent().getSerializableExtra("name") != null) {
             noteName.setText(getIntent().getSerializableExtra("name").toString());
             File file = new File(getFilesDir() + "/" + getIntent().getSerializableExtra("name").toString() + ".txt");
-            Date lastModified = new Date(file.lastModified());
-
-            Toast.makeText(this, lastModified.toString(), Toast.LENGTH_LONG).show();
+            try {
+                FileReader reader = new FileReader(file);
+                Scanner scanner = new Scanner(reader);
+                while(scanner.hasNextLine()){
+                    noteText.setText(scanner.nextLine());
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
 
